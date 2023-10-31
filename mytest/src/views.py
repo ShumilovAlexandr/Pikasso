9f55f49924238499a814cb883686994356fa72e2
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2023-11-01 01:48:39 +0300

    надо переписать

--- mytest/src/views.py
+++ mytest/src/views.py
@@ -9,18 +9,4 @@ class FileViewSet(viewsets.ModelViewSet):
     queryset = File.objects.all()
     serializer_class = FileSerializer
 
-    # def create(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     if serializer.is_valid():
-    #         self.perform_create(serializer)
-    #         headers = self.get_success_headers(serializer.data)
-    #         return Response(serializer.data,
-    #                         status=status.HTTP_201_CREATED,
-    #                         headers=headers)
-    #     else:
-    #         return Response(serializer.errors,
-    #                         status=status.HTTP_400_BAD_REQUEST)
-    #
-    # def perform_create(self, serializer):
-    #     serializer.save()
 
